c4a9b7ab8e8788dcc5d223dee4c29c8aff385580
iMplode nZ <rg@youxplode.com>
2021-05-08 20:04:58 -0700

    Removed unnecessary.

--- cpu/src/main.rs
+++ cpu/src/main.rs
@@ -1,17 +1,17 @@
+use console::Term;
 use cpu::*;
 use nalgebra::Vector2;
-use console::Term;
 
 fn main() {
     let map = Map::from_file("../assets/start.map");
     let mut world = World::new(&map);
     let term = Term::stdout();
     loop {
-        for i in 0..100 {
+        for _ in 0..100 {
             world.physics_update();
         }
         world.update_pixels();
-        term.clear_screen();
+        term.clear_screen().unwrap();
         println!("{}", world.ascii_render(Vector2::new(8, 16)));
     }
 }
